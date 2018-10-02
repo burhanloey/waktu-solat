54814efc0855d7e0382299aec2487664dc6e1168
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-10-02 12:50:23 +0800

    Add district codes

--- app/src/main/java/com/burhanloey/waktusolat/MainActivity.java
+++ app/src/main/java/com/burhanloey/waktusolat/MainActivity.java
@@ -2,12 +2,31 @@ package com.burhanloey.waktusolat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String[] districtCodes = { "JHR01", "JHR02", "JHR03", "JHR04", "KDH01",
+            "KDH02", "KDH03", "KDH04", "KDH05", "KDH06", "KDH07", "KTN01", "KTN03", "MLK01",
+            "NGS01", "NGS02", "PHG01", "PHG02", "PHG03", "PHG04", "PHG05", "PHG06", "PLS01",
+            "PNG01", "PRK01", "PRK02", "PRK03", "PRK04", "PRK05", "PRK06", "PRK07", "SBH01",
+            "SBH02", "SBH03", "SBH04", "SBH05", "SBH06", "SBH07", "SBH08", "SBH09", "SGR01",
+            "SGR02", "SGR03", "SWK01", "SWK02", "SWK03", "SWK04", "SWK05", "SWK06", "SWK07",
+            "SWK08", "SWK09", "TRG01", "TRG02", "TRG03", "TRG04", "WLY01", "WLY02" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showMessage(View view) {
+        Spinner spinner = findViewById(R.id.spinner);
+        int position = spinner.getSelectedItemPosition();
+        String selected = districtCodes[position];
+
+        Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
     }
 }
