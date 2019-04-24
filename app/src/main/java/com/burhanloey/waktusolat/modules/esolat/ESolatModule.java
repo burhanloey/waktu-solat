81c9e081f991ef8e4c740345641df4cf545fa28b
Burhanuddin Baharuddin <burhanclj@gmail.com>
2019-04-24 10:11:04 +0800

    Fix SSL handshake error. Bump version.

--- app/src/main/java/com/burhanloey/waktusolat/modules/esolat/ESolatModule.java
+++ app/src/main/java/com/burhanloey/waktusolat/modules/esolat/ESolatModule.java
@@ -3,17 +3,45 @@ package com.burhanloey.waktusolat.modules.esolat;
 import com.burhanloey.waktusolat.modules.esolat.tasks.TaskManager;
 import com.burhanloey.waktusolat.modules.storage.AppDatabase;
 import com.burhanloey.waktusolat.modules.timeformat.TimeFormatter;
+import com.burhanloey.waktusolat.tls.TLSSocketFactory;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class ESolatModule {
     @Provides
-    Retrofit provideRetrofit() {
+    TLSSocketFactory provideTlsSocketFactory() {
+        try {
+            return new TLSSocketFactory();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Provides
+    OkHttpClient provideOkHttpClient(TLSSocketFactory tlsSocketFactory) {
+        return new OkHttpClient.Builder()
+                .sslSocketFactory(tlsSocketFactory, tlsSocketFactory.getTrustManager())
+                .build();
+    }
+
+    @Provides
+    Retrofit provideRetrofit(OkHttpClient client) {
         return new Retrofit.Builder()
+                .client(client)
                 .baseUrl(ESolat.URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
@@ -35,7 +63,8 @@ public class ESolatModule {
     }
 
     @Provides
-    ESolatManager provideESolatManager(ESolatApi eSolatApi, TaskManager taskManager, TimeFormatter timeFormatter) {
+    ESolatManager provideESolatManager(ESolatApi eSolatApi, TaskManager taskManager,
+                                       TimeFormatter timeFormatter) {
         return new ESolatManager(eSolatApi, taskManager, timeFormatter);
     }
 }
